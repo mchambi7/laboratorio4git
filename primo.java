472bf2dca16cbfa025faa28dc1d7d5fc4a0468fc
JoelCp <joelcallisayapaco@gmail.com>
2026-03-17 15:36:22 -0400

    nueva version programa primo, edt, lista

--- primo.java
+++ primo.java
@@ -1,13 +1,19 @@
 public class primo {
-    public static void main(String[] args) {
-        int n = 7;
-        boolean p = true;
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                p = false;
-                break;
-            }
+    public static boolean esPrimo(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) return false;
         }
-        System.out.println(p);
+        return true;
+    }
+    public static void main(String[] args) {
+        int num = 29; 
+        if (esPrimo(num)) {
+            System.out.println(num+ "es primo");
+        } else {
+            System.out.println(num+ "no es primo.");
+        }
     }
 }
