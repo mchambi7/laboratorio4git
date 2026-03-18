79bd605cd81fd5dc166ed0bea161d1163e6799f7
RDM-FT <torrezfranz85@gmail.com>
2026-03-17 22:35:20 -0400

    solucionado

--- primo.java
+++ primo.java
@@ -1,12 +1,9 @@
 import java.util.Scanner;
-
 public class primo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         System.out.print("n:");
-        int numero = sc.nextInt();
-        
+        int numero = sc.nextInt(); 
         if (numero <= 1) {
             System.out.println(numero + " no es primo");
         } else {
@@ -16,15 +13,13 @@ public class primo {
                     esPrimo = false;
                     break;
                 }
-            }
-            
+            } 
             if (esPrimo) {
                 System.out.println(numero + " si es primo");
             } else {
                 System.out.println(numero + " no es primo");
             }
         }
-        
         sc.close();
     }
 }
